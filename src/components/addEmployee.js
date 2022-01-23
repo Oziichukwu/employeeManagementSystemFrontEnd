b2174b43e1d145d1b8cb6e85a1d5b391951b753a
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-01-24 00:24:24 +0100

    add employee link updated

--- src/components/addEmployee.js
+++ src/components/addEmployee.js
@@ -1,38 +1,81 @@
-import React, { useState  } from 'react';
+import React, { useState, useEffect  } from 'react';
 import EmployeeService from '../services/EmployeeService';
 //import { useHistory } from 'react-router-dom';
 
 
-const AddEmployee = () => {
+const AddEmployee = ({history, match}) => {
 
     const [firstName, setFirstName] = useState("")
     const [lastName, setLastName] = useState("")
     const [emailId, setEmailId] = useState("")
-    //const history = useHistory();
+    const [userName, setUserName] = useState("")
 
-    const saveEmployee = (e)=> {
+    const cancelSaveEmployeehandler= (e) => {
+
         e.preventDefault()
+
+        history.push("/employees")
         
-        const employee = {firstName , lastName, emailId}
-        EmployeeService.createEmployee(employee).then((response)=>{
-            console.log(response.data)
-           // history.push("/employees")
-
-        }).catch((err) => {
-            console.log(err)
-        })
-
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+
+    const saveOrUpdateEmployeehandler = (e)=> {
+        e.preventDefault()
+        
+        const employee = {firstName , lastName, emailId, userName}
+
+        if (match.params.id){
+            EmployeeService.updateEmployee(match.params.id, employee).then((response) => {
+
+                history.push("/employees")
+
+            }).catch((err) => {
+                console.error(err)
+            })
+
+        }else {
+            EmployeeService.createEmployee(employee).then((response)=>{
+                console.log(response.data)
+                history.push("/employees")
+    
+            }).catch((err) => {
+                console.log(err)
+            })
+        }
+    }
+
+    useEffect(() => {
+       EmployeeService.getEmployeeById(match.params.id).then((response)=>{
+           setFirstName(response.data.firstName)
+           setLastName(response.data.lastName)
+           setEmailId(response.data.emailId)
+           setUserName(response.data.userName)
+       }).catch((err) => {
+           console.log(err)
+       })
+    }, [])
+
+    const label = ()=>{
+        if (match.params.id){
+            return <h2 className="text-center">Update Employee</h2>
+        }else{
+            return <h2 className="text-center">Add Employee</h2>
+
+        }
+    }
 
     return ( <div>
         <br />
        <div className="container">
            <div className="row">
                <div className="card col-md-6 offset-md-3 offset-md-3">
-                    <h2 className="text-center">Add Employee</h2>
+                    {label()}
                     <div className="card-body">
-                        <form>
+                        <form onSubmit={(e)=>handleSubmit(e)}>
                             <div className="form-group mb-2">
                                 <label className="form-label">First Name:</label>
                                 <input type="text" placeholder="Enter First Name"
@@ -60,7 +103,17 @@ const AddEmployee = () => {
                                 onChange={(e)=>setEmailId(e.target.value)}
                                 />
                             </div>
-                            <button className="btn btn-success" onClick={(e)=>saveEmployee(e)}>Submit</button>
+                            <div className="form-group mb-2">
+                                <label className="form-label">Username:</label>
+                                <input type="text" placeholder="Enter username"
+                                name = "username"
+                                className="form-control"
+                                value = {userName}
+                                onChange={(e)=>setUserName(e.target.value)}
+                                />
+                            </div>
+                            <button className="btn btn-success" onClick={(e)=>saveOrUpdateEmployeehandler(e)}>Submit</button>
+                            <button className="btn btn-danger m-2" onClick={(e)=>cancelSaveEmployeehandler(e)}>Cancel</button>
                         </form>
                     </div>
                </div>
