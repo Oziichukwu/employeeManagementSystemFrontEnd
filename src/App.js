ca4c6a74c1477b592d15b5ebebac2e35beeea6f0
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-01-24 00:23:49 +0100

    update and get url added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import AddEmployee from './components/addEmployee'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
 import HeaderComponent from './components/headerComponent'
 import FooterComponent from './components/footerComponent'
+import './App.css';
 
 
 function App() {
   return (
     <div>
-      <HeaderComponent/>
-      <ListEmployeeComponent/>
-      <FooterComponent/>
+      
+        <HeaderComponent/>
+        <div className="container">
+          <Switch>
+            <Route exact path = "/" component = {ListEmployeeComponent}/>
+            <Route path = "/employees" component = {ListEmployeeComponent}/>
+            <Route path = "/add-employee" component = {AddEmployee}/>
+          </Switch>
+        
+        </div>
+        <FooterComponent/>
+     
     </div>
   );
 }
--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 
+
+const DELETE_EMPLOYEE_BASE_REST_API = "http://localhost:8080/api/v1"
+const GET_EMPLOYEE_BASE_REST_API_URL = "http://localhost:8080/api/v1"
 const EMPLOYEE_BASE_REST_API_URL = "http://localhost:8080/api/v1/employees";
+const CREATE_EMPLOYEE_BASE_REST_API_URL = "http://localhost:8080/api/v1/create_employee";
+const UPDATE_EMPLOYEE_BASE_REST_API_URL = "http://localhost:8080/api/v1"
+
 
 class EmployeeService {
 
@@ -8,8 +14,19 @@ class EmployeeService {
         return axios.get(EMPLOYEE_BASE_REST_API_URL)
     }
 
-    createEmployee(){
-        return axios.post(EMPLOYEE_BASE_REST_API_URL)
+    createEmployee(employee){
+        return axios.post(CREATE_EMPLOYEE_BASE_REST_API_URL, employee)
+    }
+
+    getEmployeeById(employeeId){
+        return axios.get(GET_EMPLOYEE_BASE_REST_API_URL + "/" + employeeId)
+    }
+    updateEmployee(employeeId, employee){
+        return axios.put(UPDATE_EMPLOYEE_BASE_REST_API_URL + "/" + employeeId, employee) 
+    }
+
+    deleteEmployee(employeeId){
+        return axios.delete(DELETE_EMPLOYEE_BASE_REST_API + "/" + employeeId)
     }
 }
 
